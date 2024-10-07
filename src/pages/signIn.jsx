55e6071e2fb515de941ac73dc93f0f2e9bca968f
azhuraaa <azhurareyy@gmail.com>
2024-10-07 17:53:55 +0700

    membuat page sign in dan sign up

---
+++ src/pages/signIn.jsx
@@ -0,0 +1,12 @@
+import FormSignIn from "../components/fragments/formsignln";
+import Authlayout from "../components/layout/layout";
+
+const SignIn = () => {
+  return (
+    <Authlayout title="sign in">
+      <FormSignIn />
+    </Authlayout>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/signUp.jsx
@@ -0,0 +1,12 @@
+import FormSignUp from "../components/fragment/formsignUp";
+import Authlayout from "../components/Layouts/Authlayout";
+
+const SignUp = () => {
+  return (
+    <Authlayout title="sign up">
+      <FormSignUp />
+    </Authlayout>
+  );
+};
+
+export default SignUp;
