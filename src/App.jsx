461055ab1454c0778eb4fa416d477f3720703d50
AzhuraaaReyy <azhurareyy@gmail.com>
2024-10-28 12:44:16 +0700

    update application finebank

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,59 @@ import SignInPage from "./Pages/signIn";
 import SignUpPage from "./Pages/signUp";
 import ErrorRoute from "./Pages/errorRoute";
 import ForgetPassword from "./Pages/forgetpassword";
-
+import DashboardPage from "./Pages/dashboard";
+import BalancePage from "./Pages/balance";
 
 const App = () => {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: <div>Halaman Utama</div>,
+      element: <DashboardPage />,
     },
     {
       path: "/eror",
-      element: <ErrorRoute/>
+      element: <ErrorRoute />,
     },
     {
       path: "/login",
-      element: <SignInPage/>
+      element: <SignInPage />,
     },
     {
       path: "/register",
-      element: <SignUpPage/>
+      element: <SignUpPage />,
     },
     {
       path: "/forgetpassword",
-      element: <ForgetPassword/>
+      element: <ForgetPassword />,
     },
-
+    {
+      path: "/balance",
+      element: <BalancePage />,
+    },
+    {
+      path: "/bills",
+      element: <ErrorRoute />,
+    },
+    {
+      path: "/transaction",
+      element: <ErrorRoute />,
+    },
+    {
+      path: "/expenses",
+      element: <ErrorRoute />,
+    },
+    {
+      path: "/goals",
+      element: <ErrorRoute />,
+    },
+    {
+      path: "/settings",
+      element: <ErrorRoute />,
+    },
+    {
+      path: "/logout",
+      element: <ErrorRoute />,
+    },
   ]);
 
   return (
