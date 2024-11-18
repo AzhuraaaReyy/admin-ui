7fae0e11f15489267ef3b880216374e4aa91dbb9
AzhuraaaReyy <azhurareyy@gmail.com>
2024-11-18 16:07:01 +0700

    update uts

--- src/Pages/goal.jsx
+++ src/Pages/goal.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/Elements/Card";
+import Card from "../Components/Elements/Card";
 import MainLayout from "../components/Layouts/MainLayout";
 
 const GoalPage = () => {
